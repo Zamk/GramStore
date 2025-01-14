0a74f9bb882cd0696f2b1a76c7ffd33724e3bc07
Nikita Shlyakov <nikita16370@gmail.com>
2025-01-14 06:22:13 +0300

    Add project files.

---
+++ GramStore.MenuService/Models/Organization.cs
@@ -0,0 +1,9 @@
+﻿namespace GramStore.MenuService.Models
+{
+    public class Organization
+    {
+        public long Id { get; set; }
+        public long ClientId {  get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ GramStore.MenuService/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace GramStore.MenuService.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public long OrganizationId { get; set; }
+        public long CategoryId {  get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageLink { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
