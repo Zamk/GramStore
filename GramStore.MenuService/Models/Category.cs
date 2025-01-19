266c3129696ec608b6e8bb86e555a4c7584850f1
Nikita Shlyakov <nikita16370@gmail.com>
2025-01-19 08:41:25 +0300

    remove test project

--- GramStore.MenuService/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GramStore.MenuService.Models
-{
-    public class Category
-    {
-        public long Id { get; set; }
-        public long OrganizationId { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
--- GramStore.MenuService/Models/Organization.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GramStore.MenuService.Models
-{
-    public class Organization
-    {
-        public long Id { get; set; }
-        public long ClientId {  get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
