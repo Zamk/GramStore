95dfaeed7539769bf1c3c8fcc276f0a0250a433d
Nikita Shlyakov <nikita16370@gmail.com>
2025-02-23 05:49:10 +0300

    GS-3: add GramStore.Client.Domain project & some models

---
+++ src/Client/GramStore.Client.Domain/Models/Client.cs
@@ -0,0 +1,40 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace GramStore.Client.Domain.Models
+{
+    internal class Client : Entity
+    {
+        public string Name { get; protected set; }
+        public long TelegramId { get; protected set; }
+        public Email? Email { get; protected set; }
+        public Phone? Phone { get; protected set; }
+
+
+        private Client() { }
+
+        public Client(string name, long telegramId, Email? email, Phone? phone) 
+        { 
+            Name = name;
+            TelegramId = telegramId;
+            Email = email;
+            Phone = phone;
+        }
+
+        public static Result<Client> Create(string name, long telegramId, Email? email, Phone? phone)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return Result.Failure<Client>("Name can not be null or white space");
+            }
+
+            if (telegramId == 0)
+            {
+                return Result.Failure<Client>("Name can not be 0");
+            }
+
+            var client = new Client(name, telegramId, email, phone);  
+
+            return Result.Success(client);
+        }
+    }
+}
---
+++ src/Client/GramStore.Client.Domain/Models/Email.cs
@@ -0,0 +1,33 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace GramStore.Client.Domain.Models
+{
+    public class Email : ValueObject
+    {
+        public string EmailAddress { get; protected set; } = string.Empty;
+
+        private Email() { }
+
+        private Email(string emailAddress)
+        {
+            EmailAddress = emailAddress;
+        }
+
+        public static Result<Email> Create(string emailAddress)
+        {
+            if (string.IsNullOrWhiteSpace(emailAddress))
+            {
+                return Result.Failure<Email>("Email can not be null or white space");
+            }
+
+            var email = new Email(emailAddress);
+
+            return Result.Success(email);
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return EmailAddress;
+        }
+    }
+}
